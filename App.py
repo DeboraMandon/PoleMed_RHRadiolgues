00ed6d37591dc485436422b0e8a668576c86af94
MANDON Débora <121875978+DeboraMandon@users.noreply.github.com>
2023-06-14 15:33:26 +0200

    Update App.py

--- App.py
+++ App.py
@@ -63,6 +63,7 @@ def main():
         if excel_file is not None: 
             # Charger le fichier Excel dans un DataFrame pandas
             df = pd.read_excel(excel_file)
+            df = df[df['Date'] > '2022-12-31']
             df = df[(df['Source'] == 'PDS') | (df['Source'] == 'CDS')]
         else:
             st.write("Charger votre fichier excel pour pouvoir commencer.")  
